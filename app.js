73602836a445e0a01148a98769491dc654fc9d82
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-04-24 02:12:03 +0530

    Add user registration, login, and auth middleware

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { connect } from "./utils/dbConnect";
+import { connect } from "./utils/dbConnect.js";
+
+import apiRouter from "./routes/index.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 connect();
+
+app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
